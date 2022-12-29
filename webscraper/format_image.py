286818c7bb334d9c1562e01d60c9a74973675324
Jean-François Senécal <jeanjeansenecal@gmail.com>
2022-12-28 23:22:37 -0500

    fix: allow different input

--- webscraper/format_image.py
+++ webscraper/format_image.py
@@ -4,30 +4,35 @@ import matplotlib.image as mpim
 import cv2
 
 
-for file in os.listdir('images'):
-    if file.endswith('.jpg'):
-        img = mpim.imread('images/'+file)
-        print(img.shape)
-    else:
-        continue
+# for file in os.listdir('images'):
+#     if file.endswith('.jpg'):
+#         img = mpim.imread('images/'+file)
+#         print(img.shape)
+#     else:
+#         continue
 
 
 def resize(path):
     desired_size = (224, 224)
     for file in os.listdir(path):
-        if file.endswith('.jpg'):
-            img = mpim.imread('images/'+file)
-            resized_image = cv2.resize(img, desired_size)
-            # create a new direcory in path called resized
-            os.makedirs('images/resized', exist_ok=True)
+        if file.endswith('.jpg') or file.endswith('.png'):
+            try:
+                img = mpim.imread(path+file)
+                resized_image = cv2.resize(img, desired_size)
+                # create a new direcory in path called resized
+                os.makedirs(path+'resized', exist_ok=True)
 
-            mpim.imsave('images/resized/'+file, resized_image)
+                mpim.imsave(path+'resized/'+file, resized_image)
+            except Exception as e:
+                print(e)
+                continue
 
 
-def main():
-    path = "images"
+def main(image_site):
+    path = "../images/"+image_site + "/"
     resize(path)
 
 
 if __name__ == '__main__':
-    main()
+    image_site = input("Enter the site name: ")
+    main(image_site)
